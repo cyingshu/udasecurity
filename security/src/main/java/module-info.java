fa794fc36f54ff0ae3e596fa230545a2acd9e5a6
cyingshu <cyingshu@gmail.com>
2021-11-14 11:30:09 -0800

    v2

--- security/src/main/java/module-info.java
+++ security/src/main/java/module-info.java
@@ -5,11 +5,6 @@ module security {
     requires com.google.common;
     requires java.prefs;
     requires com.google.gson;
-    requires org.slf4j;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.core;
-    requires software.amazon.awssdk.regions;
-    requires software.amazon.awssdk.services.rekognition;
 
     opens data to com.google.gson;
 }
--- security/src/test/java/service/SecurityServiceTest.java
+++ security/src/test/java/service/SecurityServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
